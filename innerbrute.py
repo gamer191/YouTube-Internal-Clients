ce1a84f4e671910f73e2976e27c0afb9b30d5a3b
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-17 18:32:25 +1000

    Significantly reduce output 🙃

--- innerbrute.py
+++ innerbrute.py
@@ -57,7 +57,7 @@ if not os.path.exists('responses'):
 
 requests_failed = 0
 
-for client_name_id in [4,6,9,11,12,17,20,22,24,25,32,34,35,36,37,40,44,45,46,47,48,49,50,51,52,53,54,73,78,79,81,82,83,86,91,96,97,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,"UNKNOWN_INTERFACE"]:
+for client_name_id in [1,4,6,9,11,12,17,20,22,24,25,32,34,35,36,37,40,44,45,46,47,48,49,50,51,52,53,54,73,78,79,81,82,83,86,91,96,97,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,"UNKNOWN_INTERFACE"]:
     for client_version in client_versions:
         client_version = client_version.replace("\n", "").replace("\r", "")
         if client_version == "":
@@ -77,9 +77,9 @@ for client_name_id in [4,6,9,11,12,17,20,22,24,25,32,34,35,36,37,40,44,45,46,47,
                 try:
                     response = requests.post("https://" + host["domain"] + "/youtubei/v1/player?key=" + host["key"], data=data, headers=host["headers"], timeout=5)
 
-                    print("Response Code: " + str(response.status_code))
 
-                    if response.status_code == 200:
+                    if response.status_code != 400 and response.status_code != 404:
+                        print("ClientId: " + str(client_name_id) + " ClientVersion: " + str(client_version) + " @ " + host["domain"] +"Response Code: " + str(response.status_code))
                         out = open("responses/" + try_id + ".json", "w", encoding="utf-8")
                         out.write(response.text)
                         out.close()
@@ -87,6 +87,6 @@ for client_name_id in [4,6,9,11,12,17,20,22,24,25,32,34,35,36,37,40,44,45,46,47,
                     break
                 except Exception as ex:
                     if i == 3:
-                        requests_failed += 1
+                        print("request failed")
                     time.sleep(0.5)
                     print(ex)
