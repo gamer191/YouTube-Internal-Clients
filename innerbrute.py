a9bdccd857f6004169297cd1f31928e4b99f60da
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-19 00:30:14 +1000

    Update innerbrute.py

--- innerbrute.py
+++ innerbrute.py
@@ -84,7 +84,7 @@ for client_version in client_versions:
                         continue
                     else:
                         print("ClientId: " + str(client_name_id) + " ClientVersion: " + str(client_version) + " @ " + host["domain"] +"Response Code: " + str(response.status_code))
-                        sys.exit(111)
+                        sys.exit(response.status_code)
                 except Exception as ex:
                     print("request failed")
                     time.sleep(0.5)
