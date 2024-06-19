85085db0188bd3f58290392cc7fd257af59d98e3
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-19 22:44:47 +1000

    Remove useless comments and cleanup

--- innerbrute.py
+++ innerbrute.py
@@ -9,40 +9,40 @@ data_template = open('payloads/post_data.txt', 'r').read()
 
 innertube_hosts = [
     {
-        'video_id': 'vJz8QzO1VzQ', # normal video
+        'video_id': 'vJz8QzO1VzQ',
         'domain': 'www.youtube.com',
         'key': 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8',
         'headers': {
             'Origin': 'https://www.youtube.com',
             'Referer': 'https://www.youtube.com/',
             'Accept-Language': 'de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52'
-        }
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52',
+        },
     },
     {
-        'video_id': 'pckuS--UlV4', # video "for kids"
+        'video_id': 'pckuS--UlV4',
         'domain': 'www.youtubekids.com',
         'key': 'AIzaSyBbZV_fZ3an51sF-mvs5w37OqqbsTOzwtU',
         'headers': {
             'Origin': 'https://www.youtubekids.com',
             'Referer': 'https://www.youtubekids.com/',
             'Accept-Language': 'de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52'
-        }
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52',
+        },
     },
     {
-        'video_id': 'RY607kB2QiU', # music video
+        'video_id': 'RY607kB2QiU',
         'domain': 'music.youtube.com',
         'key': 'AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30',
         'headers': {
             'Origin': 'https://music.youtube.com',
             'Referer': 'https://music.youtube.com/',
             'Accept-Language': 'de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52'
-        }
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52',
+        },
     },
     {
-        'video_id': 'zv9NimPx3Es', # another music video
+        'video_id': 'zv9NimPx3Es',
         'domain': 'music.youtube.com',
         'key': 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8',
         'headers': {
