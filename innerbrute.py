6cb643be80fe6ed4cd062c028b25d284dfaa2fa8
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-19 18:45:38 +1000

    More cleanup

--- innerbrute.py
+++ innerbrute.py
@@ -36,19 +36,19 @@ innertube_hosts = [
         'headers': {
             'Origin': 'https://music.youtube.com',
             'Referer': 'https://music.youtube.com/',
-            "Accept-Language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52"
+            'Accept-Language': 'de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52'
         }
     },
     {
-        "video_id": "zv9NimPx3Es", # another music video
-        "domain": "music.youtube.com",
-        "key": "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8",
-        "headers": {
-            "Origin": "https://music.youtube.com",
-            "Referer": "https://music.youtube.com/",
-            "Accept-Language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52"
+        'video_id': 'zv9NimPx3Es', # another music video
+        'domain': 'music.youtube.com',
+        'key': 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8',
+        'headers': {
+            'Origin': 'https://music.youtube.com',
+            'Referer': 'https://music.youtube.com/',
+            'Accept-Language': 'de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52'
         }
     }
 ]
@@ -60,35 +60,35 @@ requests_failed = 0
 
 client_name_id = sys.argv[1]
 for counter,client_version in enumerate(client_versions, start=1):
-    client_version = client_version.replace("\n", "").replace("\r", "")
-    if client_version == "":
+    client_version = client_version.replace('\n', '').replace('\r', '')
+    if client_version == '':
         continue
 
     for i, host in enumerate(innertube_hosts):
 
-        try_id = str(client_name_id) + "_" + client_version + "_" + str(len(innertube_hosts) - i) + "_" + host["domain"] + "_" + host["key"]
+        try_id = str(client_name_id) + '_' + client_version + '_' + str(len(innertube_hosts) - i) + '_' + host['domain'] + '_' + host['key']
         
-        data = data_template.replace("%videoId%", host["video_id"]).replace('%clientName%', str(client_name_id)).replace('%clientVersion%', client_version)
+        data = data_template.replace('%videoId%', host['video_id']).replace('%clientName%', str(client_name_id)).replace('%clientVersion%', client_version)
 
-        headers = host["headers"].copy()
+        headers = host['headers'].copy()
 
         for i in range(0, 4):
             while True:
                 try:
                     response = requests.post("https://" + host["domain"] + "/youtubei/v1/player?key=" + host["key"], data=data, headers=host["headers"], timeout=5)
 
-                    print("ClientId: " + str(client_name_id) + " ClientVersion: " + str(client_version) + " @ " + host["domain"] +"Response Code: " + str(response.status_code))
+                    print('ClientId: ' + str(client_name_id) + ' ClientVersion: ' + str(client_version) + ' @ ' + host['domain'] +'Response Code: ' + str(response.status_code))
                     if response.status_code == 400 or response.status_code == 404:
                         break
-                    elif response.status_code == 502:
+                    if response.status_code == 502:
                         continue
-                    elif response.status_code == 200:
-                        print("::notice ::" + "ClientId: " + str(client_name_id) + " ClientVersion: " + str(client_version) + " @ " + host["domain"])
+                    if response.status_code == 200:
+                        print('::notice ::' + 'ClientId: ' + str(client_name_id) + ' ClientVersion: ' + str(client_version) + ' @ ' + host['domain'])
                         break
                     else:
                         sys.exit(192)
                 except Exception as ex:
-                    print("request failed")
+                    print('request failed')
                     time.sleep(0.5)
                     print(ex)
                     continue
