46c335e037e0e7e425d5d9c3ea2410ba3607fc64
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-25 16:55:58 +1000

    Update innerbrute.py

--- innerbrute.py
+++ innerbrute.py
@@ -17,7 +17,7 @@ headers = {
 
 requestset = {grequests.post('https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8', data=data_template.replace('%videoId%', 'vJz8QzO1VzQ').replace('%clientName%', str(client_number)).replace('%clientVersion%', '1'), headers=headers, timeout=5)}
 for client_version in client_versions:
-            requestlist.add(grequests.post('https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8', data=data_template.replace('%videoId%', 'vJz8QzO1VzQ').replace('%clientName%', str(client_number)).replace('%clientVersion%', client_version), headers=headers, timeout=5))
+            requestset.add(grequests.post('https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8', data=data_template.replace('%videoId%', 'vJz8QzO1VzQ').replace('%clientName%', str(client_number)).replace('%clientVersion%', client_version), headers=headers, timeout=5))
             print(client_version)
 print("requesttime")
 print(grequests.map(requestlist))
