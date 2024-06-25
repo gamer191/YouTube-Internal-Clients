8a44feaafab5fb519584bd989894847e75bd12d1
gamer191 <83270075+gamer191@users.noreply.github.com>
2024-06-25 15:17:59 +1000

    Update innerbrute.py

--- innerbrute.py
+++ innerbrute.py
@@ -33,6 +33,8 @@ for client_version in client_versions:
             except Exception:
                 print('exception')
                 continue
+            if client_version.count('.') == 1:
+                print(f'ClientVersion: {client_version} Response Code: {response.status_code}')
             if response.status_code in {400, 404}:
                 break
             if response.status_code != 502:
